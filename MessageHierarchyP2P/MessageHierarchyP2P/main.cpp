a836052a5bff5d229f781b2fcbcd71768779002a
bmccorvey3 <syndekit@gmail.com>
2014-07-23 16:03:19 -0400

    Stubbed out messages

---
+++ MessageHierarchyP2P/MessageHierarchyP2P/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MessageHierarchyP2P
+//
+//  Created by Brent on 7/22/14.
+//  Copyright (c) 2014 Brent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
--- msg/BaseMessage.cpp
+++ msg/BaseMessage.cpp
@@ -7,3 +7,11 @@
 //
 
 #include "BaseMessage.h"
+
+
+/*
+
+
+
+
+*/
