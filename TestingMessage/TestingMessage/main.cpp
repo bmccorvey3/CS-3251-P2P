db476bda3335c176fad821a98a26c39fabda9003
bmccorvey3 <syndekit@gmail.com>
2014-07-26 18:12:04 -0400

    Just refactored. About to go to dinner.

--- TestingMessage/TestingMessage/main.cpp
+++ TestingMessage/TestingMessage/main.cpp
@@ -7,6 +7,17 @@
 //
 
 #include <iostream>
+#include "BaseMessage.h"
+#include "ChooseUsernameMsg.h"
+#include "CreateChatroomMsg.h"
+#include "DestroyChatroomMsg.h"
+#include "EnterChatroomMsg.h"
+#include "LeaveChatroomMsg.h"
+#include "ListChatroomMsg.h"
+#include "NotifyDroppedPeerMsg.h"
+#include "TextMsg.h"
+#include "UpdateRecipientsMsg.h"
+
 
 int main(int argc, const char * argv[])
 {
